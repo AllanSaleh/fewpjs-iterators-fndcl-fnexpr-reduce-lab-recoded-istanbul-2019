ad3f47a5474bfb1387f36085c46cb7f8b6389921
Allan Saleh <allan@re-coded.com>
2020-06-18 09:10:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,3 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 let totalBatteries = ()=>{
     return batteriesBatch.reduce(function(acc,elem){return acc+elem},0);
 }
-
-console.log(totalBatteries())
