e9b1f745b797d6f02240ca6bf2198dd95ec314af
Allan Saleh <allan@re-coded.com>
2020-06-18 09:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let batteriesBatch = [{amount: 2},{amount: 5}, {amount: 9}]
+
+let totalBatteries = ()=>{
+    return batteriesBatch.reduce(function(acc,elem){return acc+elem.amount},0);
+}
+
+console.log(totalBatteries())
